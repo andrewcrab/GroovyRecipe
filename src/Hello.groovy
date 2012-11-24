b48d113781ce9f396c147256e1c9e41e2f5e956c
Andrew Xie <andrewxie_hr@163.com>
2012-11-24 12:51:01 +0800

    Relocation

---
+++ src/Hello.groovy
@@ -0,0 +1,13 @@
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Andrew
+ * Date: 12-11-24
+ * Time: 下午12:42
+ * To change this template use File | Settings | File Templates.
+ */
+class Hello {
+    static main (args){
+
+    }
+}
